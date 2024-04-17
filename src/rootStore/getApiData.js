77574a174d2f365d75e005343c569f600c0173bc
Mahdi209 <dr.arcadea@gmail.com>
2024-04-17 17:20:42 +0300

    create all movie

--- src/rootStore/getApiData.js
+++ src/rootStore/getApiData.js
@@ -22,3 +22,27 @@ export const getTrendingData = () => {
       });
   };
 };
+
+export const getAllMovieData = () => {
+  return function (dispatch) {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=fabd82e3b9f4e96a721b2296be7f36ad&include_adult=false&include_video=false&language=en-US&page=2&sort_by=popularity.desc"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: get_Trending, payload: data.results });
+      });
+  };
+};
+
+export const getActors = () => {
+  return function (dispatch) {
+    fetch(
+      "https://api.themoviedb.org/3/person/popular?&page=1&api_key=aee80312b2fadfe6904aa67a5d15bcf6"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: get_Actor, payload: data.results });
+      });
+  };
+};
